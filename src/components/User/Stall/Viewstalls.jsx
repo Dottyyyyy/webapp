1a55f78f142e6aa19b96fd6b8a909cdd6fc28b85
itsk2 <codonkellyn3@gmail.com>
2025-06-26 08:56:34 +0800

    update

--- src/components/User/Stall/Viewstalls.jsx
+++ src/components/User/Stall/Viewstalls.jsx
@@ -52,7 +52,7 @@ function ViewStalls() {
                                     >
                                         <div className="w-full h-40 bg-gray-300 rounded-md mb-4 flex items-center justify-center text-gray-500 text-xl">
                                             <img
-                                                src={stall.stall.stallImage.url}
+                                                src={stall?.stall?.stallImage?.url}
                                                 alt="Stall"
                                                 className="w-full h-full object-cover rounded-lg"
                                             />
