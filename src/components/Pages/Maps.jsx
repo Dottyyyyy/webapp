91afde21db9d75c25507ad2df9414b7e25f46208
JunnieYAHH <gerelitopuyos02@gmail.com>
2025-08-12 09:50:10 +0800

    update

--- src/components/Pages/Maps.jsx
+++ src/components/Pages/Maps.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const GoogleMapService = () => {
     const googleMapsEmbedUrl =
-        "https://www.google.com/maps/embed/v1/directions?origin=Technological%20University%20of%20the%20Philippines%20Taguig%20City&destination=New%20Taytay%20Public%20Market%2C%20Taytay%2C%20Rizal&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8";
+        "https://www.google.com/maps/embed/v1/place?q=H45P%2BH2H%2C%20Rizal%20Ave%2C%20Taytay%2C%201920%20Metro%20Manila%20(New%20Taytay%20Public%20Market)&key=AIzaSyBFw0Qbyq9zTFTd - tUY6dZWTgaQzuU17R8";
 
     return (
-        <section style={{ height: '300px', width: '850px' }}>
+        < section style={{ height: '300px', width: '850px' }
+        }>
             <div style={{ maxWidth: '1200px', margin: '0 auto' }}>
 
                 <div style={{ display: 'flex', flexWrap: 'wrap', gap: '2rem' }}>
@@ -26,16 +27,16 @@ const GoogleMapService = () => {
                         </div>
                         <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem', gap: '0.75rem' }}>
                             <span style={{ fontSize: '1.5rem', color: '#FF6B6B' }}>📍</span>
-                            <strong style={{color:'white'}}>New Taytay Public Market, Taytay, Rizal</strong>
+                            <strong style={{ color: 'white' }}>New Taytay Public Market, Taytay, Rizal</strong>
                         </div>
                         <div style={{ flex: '1 1 200px', display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
                             <span style={{ fontSize: '1.5rem', color: '#FFD700' }}>⏰</span>
-                            <strong style={{color:'white'}}>Open: 5AM - 11PM (DAILY)</strong>
+                            <strong style={{ color: 'white' }}>Open: 5AM - 11PM (DAILY)</strong>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 };
 
